c7c51f108c6e00342ebad5fbd9451c96d28bd452
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2019-01-07 22:35:37 +0100

    Impelement ByteReader.

--- ChunkIO/ByteReader.cs
+++ ChunkIO/ByteReader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,23 @@ namespace ChunkIO
 {
     class ByteReader : IDisposable
     {
-        public string Name => throw new NotImplementedException();
-        public long Length => throw new NotImplementedException();
-        public void Seek(long position) => throw new NotImplementedException();
-        public Task<int> Read(byte[] array, int offset, int count) => throw new NotImplementedException();
-        public void Dispose() => throw new NotImplementedException();
+        readonly FileStream _file;
+
+        public ByteReader(string fname)
+        {
+            _file = new FileStream(
+                fname,
+                FileMode.Open,
+                FileAccess.Read,
+                FileShare.ReadWrite | FileShare.Delete,
+                bufferSize: 512,
+                useAsync: true);
+        }
+
+        public string Name => _file.Name;
+        public long Length => _file.Length;
+        public void Seek(long position) => _file.Seek(position, SeekOrigin.Begin);
+        public Task<int> Read(byte[] array, int offset, int count) => _file.ReadAsync(array, offset, count);
+        public void Dispose() => _file.Dispose();
     }
 }
