aa426b33680b9cfb8d645cd3e2fce827416c975f
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2019-01-20 12:45:15 +0100

    Add license notices to all files.

--- ChunkIO/BufferedReader.cs
+++ ChunkIO/BufferedReader.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿// Copyright 2019 Roman Perepelitsa
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -106,6 +120,8 @@ namespace ChunkIO {
         Compression.DecompressTo(content, 0, content.Length, res);
       } catch {
         res.Dispose();
+        // This translation of decompression errors into missing chunks is the only reason
+        // why ReadAtPartitionAsync is implemented in BufferedReader rather than ChunkReader.
         return null;
       }
       return res;
