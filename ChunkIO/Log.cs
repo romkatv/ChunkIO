5a57aa77ebefa257d564d4b2c2e84f5d10c54450
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2019-01-24 14:22:11 +0100

    Add low-level logging.

---
+++ ChunkIO/Log.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChunkIO {
+  static class Log {
+    const bool Enabled = false;
+
+    static readonly string Dir = @"C:\ChunkIO";
+    static readonly string FilePath = Path.Combine(Dir, "chunkio.log");
+
+    static readonly object s_monitor = new object();
+
+    static Log() {
+      if (!Enabled) return;
+      try {
+        if (!Directory.Exists(Dir)) Directory.CreateDirectory(Dir);
+      } catch {
+      }
+    }
+
+    public static void Info(string msg) {
+      if (!Enabled) return;
+      try {
+        string rec = string.Format("[{0:yyyy-MM-dd HH:mm:ss.ffff} INFO] {1}\n", DateTime.UtcNow, msg);
+        lock (s_monitor) File.AppendAllText(FilePath, rec, Encoding.UTF8);
+      } catch {
+      }
+    }
+
+    public static void Info(string fmt, params object[] p) {
+      if (!Enabled) return;
+      try {
+        Info(p == null || p.Length == 0 ? fmt : string.Format(fmt, p));
+      } catch {
+      }
+    }
+  }
+}
