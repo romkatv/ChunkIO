d72bc430b1ce3328cffe8d950c31fbe99e1196a4
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2019-01-28 18:32:05 +0100

    Delete spurious whitespace.

--- Benchmark/Benchmark.cs
+++ Benchmark/Benchmark.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2019 Roman Perepelitsa
+// Copyright 2019 Roman Perepelitsa
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
